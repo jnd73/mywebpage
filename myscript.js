f03a05c8f7d6c03d8de58f101ea12d98ea38fbda
jnd73 <56407622+jnd73@users.noreply.github.com>
2019-10-17 14:18:28 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,6 @@
-function clickMe(){
-  document.getElementById("button1").style.backgroundColor="red";
-}
+const button1 = document.querySelector('button');
+
+button1.addEventListener('click', event => 
+{
+  button1.style.backgroundColor = "red";
+});
